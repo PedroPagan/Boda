504bfce0d8a918ee8982f5b33b48273e675504b2
PedroPagan <perepagan@gmail.com>
2025-09-19 19:56:31 +0200

    Actualización

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -3,10 +3,10 @@ export default function FAQ(){
   const qas = [
     {q: '¿Qué tengo que llevar?', a: 'La casa tiene piscina, por lo que llevad bañador, chanclas y toalla de piscina. A parte puede que refresque o llueva.'},
     {q: '¿Tengo que llevar ropa de cama/toallas de ducha?', a: 'No, las habitaciones son habitaciones de hotel, con ropa de cama y toallas.'},
-    {q: '¿Cómo me visto?', a: 'No hay un codigo de vestimenta especifico, si podeis venir con camisa mejor, pero como esteis cómodos.'},
+    {q: '¿Cómo me visto?', a: 'No hay un código de vestimenta específico, si podéis venir con camisa mejor, pero como estéis cómodos.'},
     {q: '¿Puedo llevar a mi mascota?', a: 'No, el castillo no admite mascotas :(.'},
     {q: '¿Puedo saltarme la ceremonia e ir sólo a comer el pastel?', a: 'Deberías.'},
-    {q: '¿Es necesario conjuntar la pajarita con los calcetines?', a: 'Es casi imprescindible excepto si llevas pajarita de madera, en cuyo caso puedes llevar zuecos.'},
+    {q: '¿Es necesario conjuntar el color de la pajarita con el de los calcetines?', a: 'Es casi imprescindible excepto si llevas pajarita de madera, en cuyo caso puedes llevar zuecos.'},
     {q: '¿Puedo ir vestido de blanco?', a: 'Solo los hombres pueden llevar traje blanco, en cuyo caso debe ir acompañado de cadenas de oro de 250 gr mínimo.'}
   ];
 
