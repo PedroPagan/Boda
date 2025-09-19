526aec9be4c3bb3a5e69f2216fa823880b83acc6
PedroPagan <perepagan@gmail.com>
2025-09-19 12:47:38 +0200

    Actualización

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header(){
     <div className="flex items-center gap-4">
       <div className="w-16 h-16 rounded-full bg-gradient-to-tr from-pink-400 to-indigo-500 flex items-center justify-center text-white text-xl font-bold">A & J</div>
       <div>
-        <h1 className="text-2xl lg:text-3xl font-extrabold">Boda de Ana & Javier</h1>
+        <h1 className="text-2xl lg:text-3xl font-extrabold">Boda de Irene y Pere</h1>
         <p className="text-sm text-gray-500">Te invitamos a celebrar con nosotros</p>
       </div>
     </div>
